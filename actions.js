7e0364a7ce510e111eaa6886c9090d3e5b95a864
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-08-07 12:29:50 +0100

    commented out mouseover eventlistener

--- actions.js
+++ actions.js
@@ -1,10 +1,10 @@
-const inputs = document.querySelectorAll(".controls input");
+const inputs = document.querySelectorAll(".controls input"); 
 
 function handleUpdate(){
  const size = this.dataset.sizing || "";
  document.documentElement.style.setProperty(`--${this.name}`, this.value+size);
 
 }
-
+// because inputs, the outcome result of querySelectorAll is a NodeList
 inputs.forEach(input => input.addEventListener("change", handleUpdate));
-inputs.forEach(input =>input.addEventListener("mousemove", handleUpdate));
+// inputs.forEach(input =>input.addEventListener("mousemove", handleUpdate));
